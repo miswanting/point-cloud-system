7b68b88a64c7ac071e8ca7a8a0a81b010a0ab089
Miswanting <453542772@qq.com>
2019-04-24 13:23:12 +0800

    实现

--- src/pcc/pcc.go
+++ src/pcc/pcc.go
@@ -5,39 +5,63 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
+	"strconv"
 )
 
 const (
 	PORT        = 80
 	StopPattern = "\r\n\r\n"
 )
-
 func main() {
-	fmt.Println("正在建立主机...")
-	l, err := net.Listen("tcp", ":80")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer l.Close()
-	fmt.Println("主机建立完成！")
+	currentPort:=PORT
+
 	for {
-		// 等待接入
-		fmt.Println("正在等待连接...")
-		conn, err := l.Accept()
+		fmt.Println("[INFO]","正在尝试侦听",currentPort,"端口...")
+		l, err := net.Listen("tcp", ":"+strconv.Itoa(currentPort))
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println("[ERRO]",currentPort,"端口已被占用！")
+			currentPort+=1
+			continue
+		}else {
+			defer l.Close()
+			for {
+				// 等待接入
+				fmt.Println("[INFO]","正在侦听",currentPort,"端口...")
+				conn, err := l.Accept()
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println("[FINE]","已通过",conn.LocalAddr(),"的地址接收到来自",conn.RemoteAddr(),"的连接！")
+				// 在新的Go程里处理会话
+				// 循环返回到等待新接入，就可以用协程处理接入
+				go func(c net.Conn) {
+					reader := bufio.NewReader(conn)
+					writer := bufio.NewWriter(conn)
+					readwriter := bufio.NewReadWriter(reader, writer)
+					msg := ReadString(readwriter)
+					fmt.Println(msg)
+					WriteString(readwriter,msg)
+					c.Close()
+					fmt.Println("[INFO]","连接已关闭！")
+				}(conn)
+			}
 		}
-		fmt.Println("接收到连接！")
-		// 在新的Go程里处理会话
-		// 循环返回到等待新接入，就可以用协程处理接入
-		go func(c net.Conn) {
-			reader := bufio.NewReader(conn)
-			writer := bufio.NewWriter(conn)
-			readwriter := bufio.NewReadWriter(reader, writer)
-			readwriter.Read()
-			fmt.Println()
-			c.Close()
-			fmt.Println("连接已关闭！")
-		}(conn)
 	}
+}
+
+func Read(readwriter *bufio.ReadWriter)(b []byte)  {
+	return
+}
+func Write(readwriter *bufio.ReadWriter,b []byte)  {
+	
+}
+func ReadString(readwriter *bufio.ReadWriter)(str string)  {
+	raw_msg, _ := readwriter.ReadString('\n')
+	msg:=strings.Split(raw_msg, "\n")
+	return msg[0]
+}
+func WriteString(readwriter *bufio.ReadWriter,str string)  {
+	readwriter.WriteString(str+"\n")
+	readwriter.Flush()
 }
--- src/pct/pct.go
+++ src/pct/pct.go
@@ -3,12 +3,39 @@ package main
 import (
 	"log"
 	"net"
+	"bufio"
+	"fmt"
+	"strings"
 )
 
 func main() {
+	fmt.Println("[INFO]","正在拨号...")
 	conn, err := net.Dial("tcp", ":80")
 	if err != nil {
 		log.Fatal(err)
 	}
-	conn.Write([]byte("text" + "\n"))
+	fmt.Println("[FINE]","已通过",conn.LocalAddr(),"的地址连接到",conn.RemoteAddr(),"的主机！")
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
+	readwriter := bufio.NewReadWriter(reader, writer)
+	WriteString(readwriter,"123")
+	fmt.Println("msg")
+	msg := ReadString(readwriter)
+	fmt.Println(msg)
+}
+
+func Read(readwriter *bufio.ReadWriter)(b []byte)  {
+	return
+}
+func Write(readwriter *bufio.ReadWriter,b []byte)  {
+	
+}
+func ReadString(readwriter *bufio.ReadWriter)(str string)  {
+	raw_msg, _ := readwriter.ReadString('\n')
+	msg:=strings.Split(raw_msg, "\n")
+	return msg[0]
+}
+func WriteString(readwriter *bufio.ReadWriter,str string)  {
+	readwriter.WriteString(str+"\n")
+	readwriter.Flush()
 }
